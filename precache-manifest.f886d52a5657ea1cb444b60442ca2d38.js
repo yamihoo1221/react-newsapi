16e75507c57b7699f65d149acee094ba395f3a1b
鈴木郁弥 <suzukifumiya@MacBook-Air.local>
2020-09-10 18:01:16 +0900

    Updates

---
+++ precache-manifest.f886d52a5657ea1cb444b60442ca2d38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dbe54683a28d9d1939650dbbf425a26",
+    "url": "/react-newsapi/index.html"
+  },
+  {
+    "revision": "3acea4cadb75b92903bb",
+    "url": "/react-newsapi/static/css/main.19ea3f40.chunk.css"
+  },
+  {
+    "revision": "32d62cb66ce585decbfc",
+    "url": "/react-newsapi/static/js/2.c107e149.chunk.js"
+  },
+  {
+    "revision": "bc0495e3914ed396093e7a7990e70b00",
+    "url": "/react-newsapi/static/js/2.c107e149.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3acea4cadb75b92903bb",
+    "url": "/react-newsapi/static/js/main.9279b73b.chunk.js"
+  },
+  {
+    "revision": "2bfa13107f8d979d40b0",
+    "url": "/react-newsapi/static/js/runtime-main.78fc3f54.js"
+  }
+]);
